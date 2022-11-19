664fd8d072182f59c01cee2eee80895c04d2385f
luizfilipe29 <luiz_filipe.29@hotmail.com>
2022-11-19 20:15:57 -0300

    Project Created

---
+++ backend/src/test/java/com/luizfilipe/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizfilipe.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
