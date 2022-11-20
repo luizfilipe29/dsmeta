dfc593d4da06223fa921a3ff73aeae7961602ff1
luizfilipe29 <luiz_filipe.29@hotmail.com>
2022-11-19 21:07:51 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,12 @@
 import { useState } from 'react'
+import NotificationButton from './components/NotificationButton'
 
 function App() {
   return(
+    <>
     <h1>Olá mundo!</h1>
+    <NotificationButton/>
+    </>
   )
 }
 
